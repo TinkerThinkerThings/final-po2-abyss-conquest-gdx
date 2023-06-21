77531aec863cf5ef11724a599b2b6474d43f3384
Meepoo <desdjaja@yahoo.com>
2023-06-21 14:46:41 +0700

    change setting welcomescreen

--- core/src/com/gdx/abyssconquest/screens/WelcomeScreen.java
+++ core/src/com/gdx/abyssconquest/screens/WelcomeScreen.java
@@ -2,16 +2,16 @@ package com.gdx.abyssconquest.screens;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class WelcomeScreen extends AbyssScreen {
-  SpriteBatch batch;
-  TextureRegion wcscTexture;
-  float time = 0;
+  private SpriteBatch batch;
+  private TextureRegion wcscTexture;
+  private float alpha = 0;
+  private final float fadeInDuration = 4f;
 
   public WelcomeScreen(Game game) {
     super(game);
@@ -21,21 +21,23 @@ public class WelcomeScreen extends AbyssScreen {
   public void show() {
     wcscTexture = new TextureRegion(new Texture(Gdx.files.internal("assets/images/abyss_conquest.png.png")));
     batch = new SpriteBatch();
+    alpha = 0;
   }
 
   @Override
   public void render(float delta) {
     Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    alpha += delta / fadeInDuration;
+
     batch.begin();
+    batch.setColor(1, 1, 1, alpha);
     batch.draw(wcscTexture, 0, 0);
+    batch.setColor(1, 1, 1, 1);
     batch.end();
 
-    time += delta;
-    if (time > 1) {
-      if (Gdx.input.isKeyPressed(Keys.ANY_KEY) || Gdx.input.justTouched()) {
-        // game.setScreen(new GameScreen(game));
-      }
-    }
+    // if (time > 1) {
+    // if (Gdx.input.isKeyPressed(Keys.ANY_K || Gdx.input.justTouched()) {
+    // game.setScreen(new GameScreen(game)); }
   }
 
   @Override
