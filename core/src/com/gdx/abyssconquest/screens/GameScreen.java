3aff5e7dc4514cc8bda36cdb4a9f3b1ba83f1735
Meepoo <desdjaja@yahoo.com>
2023-06-21 19:47:34 +0700

    adding zombie object at WelcomeScreen

---
+++ core/src/com/gdx/abyssconquest/screens/GameScreen.java
@@ -0,0 +1,17 @@
+// package com.gdx.abyssconquest.screens;
+
+// public class GameScreen extends AbyssScreen {
+// public GameScreen(Game game) {
+// super(game);
+// }
+
+// @Override
+// public void show() {
+
+// }
+
+// @Override
+// public void render(float delta) {
+
+// }
+// }
--- core/src/com/gdx/abyssconquest/screens/WelcomeScreen.java
+++ core/src/com/gdx/abyssconquest/screens/WelcomeScreen.java
@@ -6,44 +6,54 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.gdx.abyssconquest.Zombie;
 
 public class WelcomeScreen extends AbyssScreen {
-  private SpriteBatch batch;
-  private TextureRegion wcscTexture;
-  private float alpha = 0;
-  private final float fadeInDuration = 4f;
+    private SpriteBatch batch;
+    private TextureRegion wcscTexture;
+    private float alpha = 0;
+    private final float fadeInDuration = 4f;
+    private Zombie zombie;
 
-  public WelcomeScreen(Game game) {
-    super(game);
-  }
+    public WelcomeScreen(Game game) {
+        super(game);
+    }
 
-  @Override
-  public void show() {
-    wcscTexture = new TextureRegion(new Texture(Gdx.files.internal("assets/images/abyss_conquest.png.png")));
-    batch = new SpriteBatch();
-    alpha = 0;
-  }
+    @Override
+    public void show() {
+        wcscTexture = new TextureRegion(new Texture(Gdx.files.internal("assets/images/abyss_conquest.png.png")));
+        batch = new SpriteBatch();
+        alpha = 0;
 
-  @Override
-  public void render(float delta) {
-    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-    alpha += delta / fadeInDuration;
+        // Membuat objek Zombie
+        zombie = new Zombie(350, 100, 64, 64, "assets/images/Zombie/Kanan/DiamKanan.png", 100);
+    }
 
-    batch.begin();
-    batch.setColor(1, 1, 1, alpha);
-    batch.draw(wcscTexture, 0, 0);
-    batch.setColor(1, 1, 1, 1);
-    batch.end();
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        alpha += delta / fadeInDuration;
 
-    // if (time > 1) {
-    // if (Gdx.input.isKeyPressed(Keys.ANY_K || Gdx.input.justTouched()) {
-    // game.setScreen(new GameScreen(game)); }
-  }
+        batch.begin();
+        batch.setColor(1, 1, 1, alpha);
+        batch.draw(wcscTexture, 0, 0);
+        batch.setColor(1, 1, 1, 1);
 
-  @Override
-  public void hide() {
-    Gdx.app.debug("AbyssConquest", "dispose WelcomeScreen");
-    batch.dispose();
-    wcscTexture.getTexture().dispose();
-  }
+        // Merender karakter Zombie
+        zombie.update(delta);
+        zombie.render(batch);
+
+        batch.end();
+
+        // if (time > 1) {
+        // if (Gdx.input.isKeyPressed(Keys.ANY_K || Gdx.input.justTouched()) {
+        // game.setScreen(new GameScreen(game)); }
+    }
+
+    @Override
+    public void hide() {
+        Gdx.app.debug("AbyssConquest", "dispose WelcomeScreen");
+        batch.dispose();
+        wcscTexture.getTexture().dispose();
+    }
 }
